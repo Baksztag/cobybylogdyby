d8e57be09d34394f9a90cf16977a587ea9a20b52
Baksztag <jakub.a.kret@gmail.com>
2017-02-18 13:23:51 +0100

    Add GameControls class

---
+++ src/main/java/GameControls.java
@@ -0,0 +1,21 @@
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by jakub.a.kret@gmail.com on 2017-02-18.
+ */
+public class GameControls {
+    private List<Map<Session, String>> rooms;
+    private List<String> roomNames;
+    private Map<Session, String> lobby;
+
+    public GameControls() {
+        this.rooms = new LinkedList<Map<Session, String>>();
+        this.roomNames = new LinkedList<String>();
+        this.lobby = new ConcurrentHashMap<Session, String>();
+    }
+}
